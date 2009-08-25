d78fc5349146d2dced10ab4764c63f31afae8d3a
alex_google <alex_google@mofo.ca>
2009-08-25 17:38:04 +0000

    updated zzz to use packaged date (which is set by R when packing, or building a binary)

--- PBSadmbwin/R/zzz.r
+++ PBSadmbwin/R/zzz.r
@@ -1,34 +1,21 @@
 .First.lib <- function(lib,pkg)
 {
-	#pkg_info <- utils::sessionInfo( package="PBSadmbwin" )$otherPkgs$PBSadmbwin
-	#pkg_date <- strsplit( pkg_info$Packaged, " " )[[1]][1]
+	pkg_info <- utils::sessionInfo( package=pkg )$otherPkgs[[ pkg ]]
+	pkg_date <- strsplit( pkg_info$Packaged, " " )[[1]][1]
 	
-	#userguide_path <- system.file( "doc/PBSadmbwin-UG.pdf", package = "PBSadmbwin" )
+	userguide_path <- system.file( "doc/PBSadmbwin-UG.pdf", package = "PBSadmbwin" )
 	
 	cat("
-PBS ADMB for Windows 0.50.13 -- Copyright (C) 2008-09 Fisheries and Oceans Canada
+PBS ADMB for Windows", pkg_info$Version, "-- Copyright (C) 2008-09 Fisheries and Oceans Canada
 
 A complete user guide 'PBSadmbwin-UG.pdf' is located at 
-.../library/PBSadmbwin/doc/PBSadmbwin-UG.pdf
-in the R library tree.
+", userguide_path, "
 
-Packaged on 2009-08-14
+Packaged on", pkg_date, "
 Pacific Biological Station, Nanaimo
 
 Type admb() to start a GUI for operating ADMB.
 
+
 ")
-#	cat("
-#PBS ADMB for Windows", pkg_info$Version, "-- Copyright (C) 2008-09 Fisheries and Oceans Canada
-#
-#A complete user guide 'PBSadmbwin-UG.pdf' is located at 
-#", userguide_path, "
-#
-#Packaged on", pkg_date, "
-#Pacific Biological Station, Nanaimo
-#
-#Type admb() to start a GUI for operating ADMB.
-#
-#
-#")
 }
