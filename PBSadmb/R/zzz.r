56ef15614582677ed6b78f9b4d0fe6c7f558bf94
rowan.haigh <rowan.haigh@dfo-mpo.gc.ca>
2012-06-05 21:55:06 +0000

    Modified function 'runAD' to remove trailing whitespace if the executable has no arguments. Modified GUI so that user can view the window description file and the project's R code.

--- PBSadmb/R/zzz.r
+++ PBSadmb/R/zzz.r
@@ -5,13 +5,13 @@
 	if( is.character( pkg_info$Packaged ) )
 		pkg_date <- strsplit( pkg_info$Packaged, " " )[[1]][1]
 	else
-		pkg_date  <- "unkown"
+		pkg_date  <- date()
 	
 	userguide_path <- system.file( "doc/PBSadmb-UG.pdf", package = "PBSadmb" )
 	
 	packageStartupMessage("
 -----------------------------------------------------------
-PBS ADMB ", pkg_info$Version, " -- Copyright (C) 2008-2011 Fisheries and Oceans Canada
+PBS ADMB ", pkg_info$Version, " -- Copyright (C) 2008-2012 Fisheries and Oceans Canada
 
 A complete user guide 'PBSadmb-UG.pdf' is located at 
 ", userguide_path, "
@@ -21,7 +21,6 @@ Pacific Biological Station, Nanaimo
 
 Type admb() to start a GUI for operating ADMB.
 -----------------------------------------------------------
-
 
 ")
 }
